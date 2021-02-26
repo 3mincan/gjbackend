9d7e5bfed842a4d8437fc390c134c2f9e231790f
Emincan Demirkaya <emincan@fireflyon.com>
2021-02-26 21:30:35 +0300

    remove fastify add express

---
+++ src/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const axios = require("axios");
+
+app.get("/positions", (req, res) => {
+  const API = "https://jobs.github.com/positions.json";
+  axios(API)
+    .then((response) => {
+      console.log(response.data);
+      res.json(response.data);
+    })
+    .catch((err) => {
+      res.send("errr!!!");
+    });
+});
+
+app.listen(port, () =>
+  console.log(`Server running on http://localhost:${port}`)
+);
