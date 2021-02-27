f488b79823f467644f271d890152967c0f582987
Emincan Demirkaya <emincan@fireflyon.com>
2021-02-27 15:56:04 +0300

    add query for spesific id

--- src/app.ts
+++ src/app.ts
@@ -6,14 +6,7 @@ const app = express();
 app.use(cors());
 
 
-const url = "https://jobs.github.com/positions.json";
-const config = {
-  url,
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-  },
-};
+const url = "https://jobs.github.com/";
 
 const port = process.env.PORT || 3000;
 
@@ -23,8 +16,30 @@ app.get("/about", (req, res) => res.send("About Page Route"));
 
 app.get("/api", (req, res) => res.status(200).json({ data: "api" }));
 
-app.get("/api/positions", (req, res) => {
-  axios(config)
+app.get("/api/positions.json", (req, res) => {
+  axios({
+    url: url + "positions.json",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+    },
+  })
+    .then((response) => {
+      console.log(response.data);
+      res.json(response.data);
+    })
+    .catch((err) => {
+      res.send("errr!!!");
+    });
+});
+
+app.get("/api/positions/:id.json", (req, res) => {
+      let id = req.params.id;
+      console.log(id)
+
+  axios({
+    url: url + `positions/${id}.json`,
+  })
     .then((response) => {
       console.log(response.data);
       res.json(response.data);
